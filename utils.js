e095da9764efc9c006db3c638cff5d0c2e26eb41
lamspecial <wkralwhwsh03@gmail.com>
2026-07-02 15:23:12 +0300

    Delete utils.js

--- utils.js
+++
@@ -1,6 +0,0 @@
-// دوال مساعدة مشتركة
-export function escapeHtml(str) {
-  return String(str).replace(/[&<>"']/g, m => ({
-    '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;'
-  }[m]));
-}
